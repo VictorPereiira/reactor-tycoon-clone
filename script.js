8aa6a8fa2afba86dfca5f9861fb1db0f9ef5e25a
victor <victor.psa02@gmail.com>
2021-08-18 23:15:16 -0300

    logic_1

--- script.js
+++ script.js
@@ -1,62 +1,144 @@
-const assert = require('assert');
+reactor_tycoon = {
 
-var game = function (id) {
-    var component = [
+    controlPanel: {
+        'cash': 1,
+        'energyValue': 0,
+        'energyCapacity': 200,
+        'search': 0
+    },
+
+    map: [
         {
-            'Name': 'WindTurbine',
-            'UnitValue': 1,
-            'Production': 1,
-            'LifeTime': 5
+            'size': 10,
+            'trees': 2,
+            'mountains': 2,
+            'building_site': [],
+            'state': 'open',
+            'value_island': 0
         }
-    ]
+    ],
 
-    var box = {
-        'Cash': 1,
-        'Energy': {
-            'Value': 0,
-            'Capacity': 200
+    mapOn: [],
+
+    shopping: {
+        box: [
+            {
+                'Name': 'Wind Turbine',
+                'UnitValue': 1,
+                'Production': 1,
+                'LifeTime': 5
+            }
+        ],
+
+        buy(id, quantity = 1) {
+            let game = reactor_tycoon,
+                elementBuy = this.box[id],
+                cashAccess = game.controlPanel.cash,
+                value = elementBuy.UnitValue * quantity
+
+            console.log(`you have ${game.controlPanel.cash} in cash!!!`)
+
+
+            if (cashAccess >= value) {
+                console.log('buying...')
+                console.log(' ');
+
+                cashAccess -= value
+                game.controlPanel.cash -= value
+                localStorage.setItem('cash', cashAccess)
+
+
+                this.putOnTheMap(elementBuy)
+            }
+        },
+
+        putOnTheMap(elementBuy) {
+            let currentMap = JSON.parse(localStorage.mapOn),
+                lifeTimeElement = elementBuy.LifeTime
+
+            currentMap.building_site.push(elementBuy)
+            reactor_tycoon.element.toOnElement(lifeTimeElement, currentMap.building_site)
+            console.log(currentMap.building_site)
         }
+    },
+
+    element: {
+        toOnElement(lifeTimeElement, buildingSiteMap) {
+            if (buildingSiteMap < 1) return
+            let game = reactor_tycoon,
+                energyValue = game.controlPanel.energyValue,
+                energyCapacity = game.controlPanel.energyCapacity
+
+            const cpt = setInterval(() => {
+                if (energyValue === energyCapacity) return
+                else energyValue++
+
+                lifeTimeElement--
+
+                console.log(`Energy_Value: ${energyValue}`)
+                console.log(`Life_Time_Component: ${lifeTimeElement}`)
+                console.log(' ')
+
+                if (lifeTimeElement < 1) {
+                    clearInterval(cpt)
+                    // 
+                    // this.component[0].LifeTime = 5
+                    // reactor_tycoon.toOnComponent()
+                }
+            }, 1000)
+        }
+    },
+
+    actions: {
+        converterEnergyToCash() {
+            if (this.energyValue < 1) return
+
+            this.cash = this.energyValue
+            this.energyValue = 0
+
+            console.log(this.cash)
+        }
+
+        // Admin Functions 
+        // addElement(imgEL, nameEL, unitValueEL, productionEL, lifeTimeEl, levelEL = 1) {
+        //     if (typeof (imgEl) !== string) return
+        //     if (typeof (nameEl) !== string) return
+
+        //     let newElement = {
+        //         'Img': imgEL,
+        //         'Name': nameEL,
+        //         'UnitValue': unitValueEL,
+        //         'Production': productionEL,
+        //         'LifeTime': lifeTimeEl,
+        //         'level': levelEL
+        //     }
+        //     this.box.dataShopping.push(newElement)
+        // }
+    },
+
+    // Game functions
+    initGame() {
+        console.log('initGame...')
+
+        if (localStorage.getItem('cash') === null) {
+            localStorage.setItem('cash', this.controlPanel.cash)
+            localStorage.setItem('mapOn', JSON.stringify(this.map[0]))
+
+            console.log('put start configs...');
+        } else {
+            this.controlPanel.cash = localStorage.cash
+            this.mapOn = JSON.parse(localStorage.mapOn)
+
+            document.querySelector('.cash').textContent = localStorage.cash
+            console.log('get configs data...');
+        }
+
+        console.log(' ');
     }
 
-    var map = [];
-
-    function buy(id) {
-        let cash = box.Cash,
-            cName = component[id].Name,
-            unitValue = component[id].UnitValue;
-
-        cash >= unitValue ? cash -= unitValue : cash;
-
-        box.Cash = cash;
-        map.push(id);
-
-        countEnergy();
-
-        function countEnergy(id) {
-            let life = component[id].LifeTime,
-                production = component[id].Production,
-                resultEnergy = production * life;
-
-            box.Energy.Value = resultEnergy;
-        }
-    }
-
-
-
-    function converterEnergyToCash(params) {
-
-    }
-
-    buy(id)
-    return box
 }
 
+reactor_tycoon.initGame()
+reactor_tycoon.shopping.buy(0)
 
-try {
-    let res;
 
-    assert.strictEqual(res, game(0))
-
-} catch (error) {
-    console.log(error);
-}
---
+++ shopping.js
@@ -0,0 +1,94 @@
+shopping = {
+    cash: 0,
+    box: [
+        {
+            'name': 'element_1',
+            'value': 5
+        },
+        {
+            'name': 'element_2',
+            'value': 10
+        },
+        {
+            'name': 'element_3',
+            'value': 30
+        }
+    ],
+    map: [],
+
+
+    // Game functions
+    initGame() {
+        if (localStorage.getItem('cash') === null) {
+            localStorage.setItem('cash', 0)
+            localStorage.setItem('map', JSON.stringify(this.map))
+        } else {
+            this.cash = localStorage.cash
+            this.map = JSON.parse(localStorage.map)
+            document.querySelector('.cash').textContent = localStorage.cash
+        }
+    },
+
+    startGame() {
+        let time = setInterval(() => {
+            this.cash++
+            localStorage.setItem('cash', this.cash)
+            document.querySelector('.cash').textContent = this.cash
+        }, 1000)
+
+        document.querySelector('.startButton').style.display = 'none'
+        document.querySelector('p').style.display = 'block'
+
+
+        for (let i in this.box) {
+            let nameEl = this.box[i].name,
+                valueEl = this.box[i].value
+
+            document.querySelector('.shopping').innerHTML += `
+            <div class="elementBuy_container" onclick="shopping.buy(${i}, 1)">
+                <h3>${nameEl}</h3>
+                <p>${valueEl}</p>
+            </div>
+        `
+        }
+
+        for (let x in this.map) {
+            let nameElement = this.map[x].name
+
+            document.querySelector('.map').innerHTML += `
+            <div>
+                <div>${nameElement}</div>
+            </div>
+        `
+        }
+    },
+
+    draw(id) {
+        let nameElement = this.map[id].name
+
+        document.querySelector('.map').innerHTML += `
+            <div>
+                <div>${nameElement}</div>
+            </div>
+        `
+    },
+
+    // Player functions
+    buy(id, quantity) {
+        let elementBuy = this.box[id],
+            value = elementBuy.value * quantity
+
+        if (this.cash >= value) {
+            this.cash -= value
+            localStorage.setItem('cash', this.cash)
+
+            while (quantity > 0) {
+                quantity--
+                this.map.push(elementBuy)
+                this.draw(id)
+                localStorage.setItem('map', JSON.stringify(this.map))
+            }
+        }
+    }
+}
+
